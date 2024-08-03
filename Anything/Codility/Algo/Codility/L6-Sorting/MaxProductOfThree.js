1740aaa23cf864b73e80d0cf1eb18fea59802c66
Prashant Sahni <prashant.sahni5@gmail.com>
2024-08-03 20:00:11 +0530

    Reset

--- Anything/Codility/Algo/Codility/L6-Sorting/MaxProductOfThree.js
+++
@@ -1,7 +0,0 @@
-function solution(A) {
-    let sorted = A.sort((a, b) => a-b);
-    let max1 = A[A.length - 1] * A[A.length - 2] * A[A.length - 3];
-    let max2 = A[0] * A[1] * A[A.length - 1];
-
-    return Math.max(max1, max2);
-}
--- stacks/main.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func Pop(a []int) []int {
-	return a[:len(a)-1]
-}
